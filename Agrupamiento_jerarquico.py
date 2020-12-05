aadd3ce26f7a51bae761ec4acbfafbbe578e6dbb
simio <yugipoi@gmail.com>
2020-12-05 11:55:59 -0500

    funcionando 1

--- Agrupamiento_jerarquico.py
+++ Agrupamiento_jerarquico.py
@@ -1,5 +1,5 @@
 import random
-from bokeh.plotting import figure, show  
+from bokeh.plotting import figure , show , output_file
 
 def random_generate(max_x, max_y, quantity):
     dots = []
@@ -10,19 +10,25 @@ def random_generate(max_x, max_y, quantity):
         dot = (x_cord, y_cord)
 
         dots.append(dot)
-        
-    print (dots)
     return dots
 
 def plot (x,y):
-   # grafica = figure(title='Cluster hierarchy', plot_height = 400, plot_width = 400)
+   grafica = figure(title='Cluster hierarchy', plot_height = 400, plot_width = 400)
+   grafica.circle(x,y,size=10, color="navy", alpha=0.5)
+   show(grafica)
     
 
 if __name__ == "__main__":
     max_x = int(input("tamaño del eje x:"))
     max_y = int(input("tamaño del eje y:"))
     quantity = int(input("cuantos puntos desea agregar?:"))
+    cord_x = []
+    cord_y = []
 
-    random_generate(max_x, max_y, quantity)
+    dots=random_generate(max_x, max_y, quantity)
 
+    for dot in dots:
+        cord_x.append(dot[0])
+        cord_y.append(dot[1])
 
+    plot(cord_x,cord_y)
